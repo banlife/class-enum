54d05fc89007589b082fd3cdc7fc8c6c2c9aabd6
JuHyun Lee <leejaycoke@gmail.com>
2022-05-12 20:25:19 +0900

    added eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
