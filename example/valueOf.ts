ada6eb12bce3eb5de787ba5605ad02066f7a8596
JuHyun Lee <leejaycoke@gmail.com>
2022-09-21 10:39:27 +0900

    valueOf support defaultValue

--- example/valueOf.ts
+++ example/valueOf.ts
@@ -3,8 +3,10 @@ import { ClassEnum } from '../src'
 class Animal extends ClassEnum<Animal> {
   public static readonly DOG = new Animal('DOG')
   public static readonly CAT = new Animal('CAT')
+  public static readonly ETC = new Animal('ETC')
 }
 
 console.log(Animal.valueOf('DOG'))
 console.log(Animal.valueOf('CAT'))
 console.log(Animal.valueOf('PARROT'))
+console.log(Animal.valueOf("MONKEY", Animal.ETC))
